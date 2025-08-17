818b316172fbbfc0d4b78aac907b293963c62117
asad-2005 <asadsaihan2005@gmail.com>
2025-08-17 11:20:19 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function exittab(){
+    window.close();
+}
+function showdate(){
+    let now=new Date();
+    let date=now.toLocaleDateString();
+    let time=now.toLocaleTimeString();
+    document.getElementById("clock").innerHTML=date+""+time;
+    setInterval(showdate,1000);
+    showdate();
+}
