b869d15a63c67023765979dde09abf59271a8804
asad-2005 <asadsaihan2005@gmail.com>
2025-08-30 11:48:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function showdate(){
     let now=new Date();
     let date=now.toLocaleDateString();
     let time=now.toLocaleTimeString();
-    document.getElementById("clock").innerHTML=date+""+time;
-    setInterval(showdate,1000);
-    showdate();
+    document.getElementById("clock").innerHTML="DATE:"+date+"  TIME:"+time;
+    
     console.log(date,time);
 }
+window.onload = function() {
+    showdate();
+    setInterval(showdate, 1000);
+};
